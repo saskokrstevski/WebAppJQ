567380285ddefa93f3baf77f73bc78327c66ca64
Sasko Krstevski <sasko.krstevski@hotmail.com>
2022-04-15 09:43:13 +0200

    Add project files.

---
+++ WebAppJQ/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using WebAppJQ.Models;
+
+namespace WebAppJQ.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //return View();
+            CustomerEntities entities = new CustomerEntities();
+            return View(from customer in entities.Customers
+                        select customer);
+        }
+        [HttpPost]
+        public ActionResult Details(int custID)
+        {
+            CustomerEntities entities = new CustomerEntities();
+            return PartialView("Details", entities.Customers.Find(custID));
+        }
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebAppJQ/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppJQ.MvcApplication" Language="C#" %>
